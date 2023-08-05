c22c41075ad4d362ca051f9f0b3624ceaff4deca
Iamneo <techadmin@iamneo.ai>
2023-08-05 07:53:10 +0000

    Backup Commit

--- reactapp/src/tests/stopwatch.test.js
+++ reactapp/src/tests/stopwatch.test.js
@@ -11,7 +11,7 @@ describe('Testing Stopwatch', () => {
         
         act(() => {
             render(<Stopwatch />);
-            jest.advanceTimersByTime(1000);
+            jest.useFakeTimers(1000);
           });
         
         
